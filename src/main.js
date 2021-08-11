056e15bf7e9783ed46f0276c6264dc1076cbac1d
Galen Lockwood <galenlockwood@gmail.com>
2021-08-11 15:50:24 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import AllProducts from './pages/AllProducts.vue';
+import ProductDetails from './pages/ProductDetails.vue';
+import AddProduct from './pages/AddProduct.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/products' },
+        { path: '/products', component: AllProducts },
+        // We cannot use $router to get dynamic route but we can use props instead
+        { path: '/products/:pid', component: ProductDetails, props: true },
+        { path: '/products/add', component: AddProduct }
+    ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
